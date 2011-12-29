063bf56a274e210b1bb3f17df923fa911c7c7b95
twinther <tommy@winther.nu>
2011-12-29 14:08:46 +0100

    Version 2.0.0 - Updated for Eden - New channel logos - Improved error handling with option for the user to submit exceptions/bugs

--- addon.py
+++ addon.py
@@ -1,7 +1,10 @@
+import os
 import sys
 import simplejson
 import urllib2
+import buggalo
 
+import xbmcaddon
 import xbmcgui
 import xbmcplugin
 
@@ -13,23 +16,31 @@ def showChannels():
     u.close()
 
     channels = simplejson.loads(data[39:-3])
-
+    path = ADDON.getAddonInfo('path')
     for channel in channels:
-        item = xbmcgui.ListItem(channel['title'], iconImage = channel['logo'])
+        if channel.has_key('source_url'):
+            item = xbmcgui.ListItem(channel['title'], iconImage = os.path.join(path, 'resources', 'logos', channel['source_url'] + '.png'))
+        else:
+            item = xbmcgui.ListItem(channel['title'], iconImage = channel['logo'])
+
         item.setProperty('IsPlayable', 'true')
-        item.setProperty("IsLive", "true")
         item.setInfo(type = 'audio', infoLabels = {
                 'title' : channel['title']
         })
 
         if type(channel['mediaFile']) is list:
-            xbmcplugin.addDirectoryItem(HANDLE, channel['mediaFile'][0], item)
+            url = channel['mediaFile'][0]
         else:
-            xbmcplugin.addDirectoryItem(HANDLE, channel['mediaFile'], item)
+            url = channel['mediaFile']
+        xbmcplugin.addDirectoryItem(HANDLE, url + ' live=1', item)
 
     xbmcplugin.endOfDirectory(HANDLE)
 
 if __name__ == '__main__':
+    ADDON = xbmcaddon.Addon()
     HANDLE = int(sys.argv[1])
-    showChannels()
+    try:
+        showChannels()
+    except Exception:
+        buggalo.onExceptionRaised()
 
